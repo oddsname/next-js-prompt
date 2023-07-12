846492ddd710fdee86ffccd85d0a9666ab4c26d9
oddsname <vadimich2012@mail.ru>
2023-07-12 15:21:42 +0300

    created Nav component and added next auth

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import Feed from "@components/Feed"
 
 export default function Home() {
-  return (
-    <div>hello</div>
-  )
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>Discover & Share</h1>
+            <br className='max-md:hidden'/>
+            <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+            <p className='desc text-center'>
+                Promptopia is an open source AI prompting tool for modern world to discover, create and shrare creative
+                prompts
+            </p>
+
+            <Feed/>
+        </section>
+    )
 }
